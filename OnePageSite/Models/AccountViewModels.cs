849ba133aa24427297a1326704ba230804a36e0e
Iven <iwenchak@gmail.com>
2020-08-10 17:34:06 +0300

    easy commit

--- OnePageSite/Models/AccountViewModels.cs
+++ OnePageSite/Models/AccountViewModels.cs
@@ -3,41 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace OnePageSite.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
-
-    public class ExternalLoginListViewModel
-    {
-        public string ReturnUrl { get; set; }
-    }
-
-     public class VerifyCodeViewModel
-    {
-        [Required]
-        public string Provider { get; set; }
-
-        [Required]
-        [Display(Name = "Код")]
-        public string Code { get; set; }
-        public string ReturnUrl { get; set; }
-
-        [Display(Name = "Запомнить браузер?")]
-        public bool RememberBrowser { get; set; }
-
-        public bool RememberMe { get; set; }
-    }
-
-    public class ForgotViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
-
+   
     public class LoginViewModel
     {
 		[Required]
@@ -52,22 +18,5 @@ namespace OnePageSite.Models
 
         [Display(Name = "Remember me")]
         public bool RememberMe { get; set; }
-    }
-
-   
-    public class ResetPasswordViewModel
-    {
-		[Required]
-		[EmailAddress]
-		[Display(Name = "Email")]
-		public string Email { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 8)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
-
-       public string Code { get; set; }
     }   
 }
